f5f385d254350db1f5a84f50a2aa57d6d145a01c
FarmMan <kusilla16@gmail.com>
2025-08-20 08:18:31 +0300

    (20) 0.0.1 | created constructor in HavenCommand.java

--- src/main/java/net/haven/commands/HavenCommand.java
+++ src/main/java/net/haven/commands/HavenCommand.java
@@ -5,6 +5,13 @@ import net.haven.utils.MessageUtils;
 import org.bukkit.command.CommandExecutor;
 
 public class HavenCommand implements CommandExecutor {
+
     private final Haven plugin;
     private final MessageUtils messages;
+
+    public HavenCommand(Haven plugin) {
+        this.plugin = plugin;
+        this.messages = plugin.getMessageUtils();
+    }
+
 }
