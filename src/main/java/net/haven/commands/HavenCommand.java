e5343122cbae90a76265ea768e58e51113f44b96
FarmMan <kusilla16@gmail.com>
2025-08-20 08:20:39 +0300

    (21) 0.0.1 | created onCommand

--- src/main/java/net/haven/commands/HavenCommand.java
+++ src/main/java/net/haven/commands/HavenCommand.java
@@ -2,7 +2,10 @@ package net.haven.commands;
 
 import net.haven.Haven;
 import net.haven.utils.MessageUtils;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 public class HavenCommand implements CommandExecutor {
 
@@ -12,6 +15,14 @@ public class HavenCommand implements CommandExecutor {
     public HavenCommand(Haven plugin) {
         this.plugin = plugin;
         this.messages = plugin.getMessageUtils();
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (args.length == 0) {
+            messages.sendMessage(sender, "messages.usage");
+            return true;
+        }
     }
 
 }
