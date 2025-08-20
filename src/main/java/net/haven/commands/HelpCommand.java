42737bf0b84733901ef6e5bdab980fae842f63b9
FarmMan <kusilla16@gmail.com>
2025-08-20 09:56:24 +0300

    (27) 0.0.1 | added subcommands

---
+++ src/main/java/net/haven/commands/HelpCommand.java
@@ -0,0 +1,36 @@
+package net.haven.commands;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import net.haven.Haven;
+import org.jetbrains.annotations.NotNull;
+
+public class HelpCommand implements CommandExecutor {
+
+    private final Haven plugin;
+
+    public HelpCommand(Haven plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+        showHelp(sender);
+        return true;
+    }
+
+    public void showHelp(CommandSender sender) {
+        String[] helpMessages = {
+                ChatColor.GOLD + "=== " + ChatColor.GREEN + "Haven Commands" + ChatColor.GOLD + " ===",
+                ChatColor.YELLOW + "/hv setspawn" + ChatColor.GRAY + " - Set spawn point",
+                ChatColor.YELLOW + "/hv spawn" + ChatColor.GRAY + " - Teleport to spawn",
+                ChatColor.YELLOW + "/hv delspawn" + ChatColor.GRAY + " - Delete spawn point",
+                ChatColor.YELLOW + "/hv reload" + ChatColor.GRAY + " - Reload configuration",
+                ChatColor.YELLOW + "/hv help" + ChatColor.GRAY + " - Show this help"
+        };
+
+        sender.sendMessage(helpMessages);
+    }
+}
---
+++ src/main/java/net/haven/commands/handlers/CommandHandler.java
@@ -0,0 +1,63 @@
+package net.haven.commands.handlers;
+
+import net.haven.Haven;
+import net.haven.commands.*;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandHandler implements CommandExecutor {
+
+    private final Haven plugin;
+    private final SetSpawnCommand setSpawnCommand;
+    private final SpawnCommand spawnCommand;
+    private final DeleteSpawnCommand deleteSpawnCommand;
+    private final ReloadCommand reloadCommand;
+    private final HelpCommand helpCommand;
+
+    public CommandHandler(Haven plugin) {
+        this.plugin = plugin;
+        this.setSpawnCommand = new SetSpawnCommand(plugin);
+        this.spawnCommand = new SpawnCommand(plugin);
+        this.deleteSpawnCommand = new DeleteSpawnCommand(plugin);
+        this.reloadCommand = new ReloadCommand(plugin);
+        this.helpCommand = new HelpCommand(plugin);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+        if (args.length == 0) {
+            helpCommand.showHelp(sender);
+            return true;
+        }
+
+        String subCommand = args[0].toLowerCase();
+
+        switch (subCommand) {
+            case "setspawn":
+                return setSpawnCommand.onCommand(sender, cmd, label, getSubArgs(args));
+            case "spawn":
+                return spawnCommand.onCommand(sender, cmd, label, getSubArgs(args));
+            case "delspawn":
+                return deleteSpawnCommand.onCommand(sender, cmd, label, getSubArgs(args));
+            case "reload":
+                return reloadCommand.onCommand(sender, cmd, label, getSubArgs(args));
+            case "help":
+                return helpCommand;
+            default:
+                sender.sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.unknown-command", "&cUnknown command!"));
+                return true;
+        }
+    }
+
+    private String[] getSubArgs(String[] args) {
+        if (args.length <= 1) {
+            return new String[0];
+        }
+        String[] subArgs = new String[args.length - 1];
+        System.arraycopy(args, 1, subArgs, 0, subArgs.length);
+        return subArgs;
+    }
+}
