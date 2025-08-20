c44c29ec38c96f394d5704ed36b098d1b951be1c
FarmMan <kusilla16@gmail.com>
2025-08-20 16:03:24 +0300

    (6) 0.1.0 | commit (nn)

--- src/main/java/net/haven/Haven.java
+++ src/main/java/net/haven/Haven.java
@@ -2,24 +2,35 @@ package net.haven;
 
 import net.haven.commands.SpawnCommand;
 import net.haven.commands.handlers.CommandHandler;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.Objects;
 
 public final class Haven extends JavaPlugin {
+    private FileConfiguration messages;
 
     @Override
     public void onEnable() {
-        getLogger().info("Haven enabled!");
-
         Objects.requireNonNull(this.getCommand("hv")).setExecutor(new CommandHandler(this));
         Objects.requireNonNull(this.getCommand("spawn")).setExecutor(new SpawnCommand(this));
 
         saveConfig();
+
+        File messagesFile = new File(getDataFolder(), "messages.yml");
+        if (!messagesFile.exists()) {
+            saveResource("messages.yml", false);
+        }
+        messages = YamlConfiguration.loadConfiguration(messagesFile);
+
+        getLogger().info("Haven enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Haven disabled!");
     }
+
 }
---
+++ src/main/java/net/haven/commands/config/ConfigManager.java
@@ -0,0 +1,43 @@
+package net.haven.commands.config;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class ConfigManager {
+    private static FileConfiguration messagesConfig;
+
+    public static void loadMessages(JavaPlugin plugin) {
+        File messagesFile = new File(plugin.getDataFolder(), "messages.yml");
+        if (!messagesFile.exists()) {
+            plugin.saveResource("messages.yml", false);
+        }
+        messagesConfig = YamlConfiguration.loadConfiguration(messagesFile);
+    }
+
+    public static String getMessage(String path) {
+        if (messagesConfig == null) {
+            return ChatColor.RED + "Messages not loaded!";
+        }
+
+        String message = messagesConfig.getString(path);
+        if (message == null) {
+            return ChatColor.RED + "Message not found: " + path;
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    public static String getMessage(String path, Object... replacements) {
+        String message = getMessage(path);
+        for (int i = 0; i < replacements.length; i += 2) {
+            if (i + 1 < replacements.length) {
+                message = message.replace(String.valueOf(replacements[i]),
+                        String.valueOf(replacements[i + 1]));
+            }
+        }
+        return message;
+    }
+}
