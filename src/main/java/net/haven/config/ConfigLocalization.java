7098997f2af7d74e87b9d703eb775a8500e98128
FarmMan <kusilla16@gmail.com>
2025-08-22 18:47:08 +0300

    1.4.0 | added new language to localization - French | +new version

--- src/main/java/net/haven/config/ConfigLocalization.java
+++ src/main/java/net/haven/config/ConfigLocalization.java
@@ -37,6 +37,7 @@ public class ConfigLocalization {
         saveLanguageFile("ukrainian.yml");
         saveLanguageFile("spanish.yml");
         saveLanguageFile("german.yml");
+        saveLanguageFile("french.yml");
 
         languages.clear();
 
